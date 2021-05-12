0c1f57abb886c2939bda3f568aa4fa73cf590f1b
JineshMajithia <mjinesh24@gmail.com>
2021-05-12 17:17:27 +0530

    story/addNoteModel-RTT-9 changes made.

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,11 @@ import ModalComponent from './components/modal/Modal'
 import Form from "./components/Forms/AddNoteForm"
 
 const App = (): JSX.Element => {
-
   return (
     <div>
       <h1> Main Page </h1>
     </div>
   )
-
-  // let form = (
-  //   <Form />
-  // )
-
-  // return (
-  //   <ModalComponent label="Add Note" form={form}/>
-  // );
 }
 
 export default App;
