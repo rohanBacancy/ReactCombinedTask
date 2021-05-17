730b87e2ac617cd617b1ab6ca7075aa0b09b3f37
JineshMajithia <mjinesh24@gmail.com>
2021-05-17 12:37:50 +0530

    addProductForm

---
+++ src/Components/Forms/AddProductForm.tsx
@@ -0,0 +1,63 @@
+import {Form, FormGroup, Label, Input,  Button} from 'reactstrap'
+import axios from 'axios'
+
+const AddProduct = () => {
+
+    function onSubmit (event: any) {
+        event.preventDefault();
+        let userId = localStorage.getItem("ProductHuntUserId");
+        let productData = {
+            userId: userId,
+            title: (document.getElementById("title") as HTMLInputElement).value,
+            category: (document.getElementById("category") as HTMLInputElement).value,
+            description: (document.getElementById("description") as HTMLInputElement).value,
+            reviews: []
+        }
+        axios.post("https://609cc6bd04bffa001792d455.mockapi.io/products", 
+        productData
+         ).then((response) => {
+            console.log(response)
+         } )
+         .catch((error) => {
+            console.log(error)
+         })
+        
+    }
+
+    return (
+        <Form onSubmit={onSubmit} >
+            <FormGroup>
+                <Label>
+                    Title
+                </Label>
+                <Input type="text" id="title" placeholder="enter title" />
+            </FormGroup>
+            <br />
+            <FormGroup>
+                <Label>
+                    Category
+                </Label>
+                <Input type="select" id="category" defaultValue="All" >
+                <option value="Category 1" >Category 1</option>
+                <option value="Category 2" >Category 2</option>
+                <option value="Category 3" >Category 3</option>
+                <option value="Category 4" >Category 4</option>
+                <option value="Category 5" >Category 5</option>
+                </Input>
+            </FormGroup>
+            <br />
+            <FormGroup>
+                <Label>
+                    Description
+                </Label>
+                <Input type="textarea" id="description" placeholder="enter description" />
+            </FormGroup>
+            <br />
+            <FormGroup>
+                <Button color="primary" type="submit">Add Product</Button>
+            </FormGroup>
+        </Form>
+    )
+}
+
+export default AddProduct;
